500cb3d61c7877a4a6406202320b7e4df64c7674
Aman <amanp3@illinois.edu>
2022-05-23 12:13:55 -0500

    take pictures for instrinsics updated

--- CVPractice/intrinsics/undistort.py
+++ CVPractice/intrinsics/undistort.py
@@ -1,15 +1,17 @@
-#from
+#from:
 #https://docs.opencv.org/4.x/dc/dbb/tutorial_py_calibration.html
-#for
-#360 birds eye camera calibration Aman Penmetcha
+
+#adated for:
+#360 birds eye camera calibration by Aman Penmetcha
+# #Calibrate the intrinsics of a camera using a chess board.
 
 import numpy as np
-import cv2 as cv
+import cv2
 import glob
 import numpy as np
 
 # termination criteria
-criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
 objp = np.zeros((6*7,3), np.float32)
 objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
@@ -19,36 +21,36 @@ imgpoints = [] # 2d points in image plane.
 images = glob.glob(r'C:\Users\Aman\Desktop\TRINA\OPENCV\DistortionImages\*.png')
 print(len(images))
 for fname in images:
-    img = cv.imread(fname)
-    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    img = cv2.imread(fname)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # Find the chess board corners
-    ret, corners = cv.findChessboardCorners(gray, (7,6), None)
+    ret, corners = cv2.findChessboardCorners(gray, (7,6), None)
     # If found, add object points, image points (after refining them)
     if ret == True:
         objpoints.append(objp)
-        corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+        corners2 = cv2.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
         imgpoints.append(corners)
         # Draw and display the corners
-        cv.drawChessboardCorners(img, (8,6), corners2, ret)
-        cv.imshow('img', img)
-        cv.waitKey(500)
-cv.destroyAllWindows()
+        cv2.drawChessboardCorners(img, (8,6), corners2, ret)
+        cv2.imshow('img', img)
+        cv2.waitKey(500)
+cv2.destroyAllWindows()
 
-ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
 
-img = cv.imread(r'C:\Users\Aman\Desktop\TRINA\OPENCV\DistortionImages\distortedImgTest.png')
+img = cv2.imread(r'C:\Users\Aman\Desktop\TRINA\OPENCV\DistortionImages\distortedImgTest.png')
 h,  w = img.shape[:2]
-newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
 print(newcameramtx)
 
 # undistort
-dst = cv.undistort(img, mtx, dist, None, newcameramtx)
+dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
 # crop the image
 x, y, w, h = roi
 dst = dst[y:y+h, x:x+w]
-cv.imshow('og image', img)
-cv.imshow('calibration result', dst)
-cv.waitKey(0)
-cv.destroyAllWindows()
+cv2.imshow('og image', img)
+cv2.imshow('calibration result', dst)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
